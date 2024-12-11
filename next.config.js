b0d71f7b728d7af169093b72d834160203794848
YanBerdin <yandevformation@gmail.com>
2024-12-11 21:04:20 +0100

    fiximages.unsplash error with next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  /* output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+*/
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 module.exports = nextConfig;
